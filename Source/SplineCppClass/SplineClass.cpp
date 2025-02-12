c94eb7c1c17cea23ca6c873b91ee50e92cabe48f
Victor Guilherme <vitorsantos.vs358@gmail.com>
2025-02-12 11:52:38 -0300

    feat:snap to ground

--- Source/SplineCppClass/SplineClass.cpp
+++ Source/SplineCppClass/SplineClass.cpp
@@ -1,27 +1,46 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "SplineClass.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 
-// Sets default values
 ASplineClass::ASplineClass()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	SplineComponent = CreateDefaultSubobject<USplineComponent>(TEXT("SplineComponent"));
+	RootComponent = SplineComponent;
 }
 
-// Called when the game starts or when spawned
 void ASplineClass::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-// Called every frame
 void ASplineClass::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+void ASplineClass::SnapAllPointsToGround()
+{
+	if (!bSnapToGround || !SplineComponent) return;
+
+	UWorld* World = GetWorld();
+	if (!World) return;
+
+	for (int32 i = 0; i < SplineComponent->GetNumberOfSplinePoints(); i++)
+	{
+		FVector PointLocation = SplineComponent->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::World);
+		FVector Start = PointLocation + FVector(0, 0, 150);
+		FVector End = PointLocation + FVector(0, 0, -10000);
+
+		FHitResult HitResult;
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(this);
+
+		if (World->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, QueryParams))
+		{
+			SplineComponent->SetLocationAtSplinePoint(i, HitResult.ImpactPoint, ESplineCoordinateSpace::World, true);
+			SplineComponent->SetUpVectorAtSplinePoint(i, HitResult.ImpactNormal, ESplineCoordinateSpace::World, true);
+		}
+	}
+}
--- Source/SplineCppClass/SplineClass.h
+++ Source/SplineCppClass/SplineClass.h
@@ -1,26 +1,30 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/SplineComponent.h"
 #include "SplineClass.generated.h"
 
 UCLASS()
 class SPLINECPPCLASS_API ASplineClass : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	ASplineClass();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
+public:
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(VisibleAnywhere, Category = "Spline")
+	USplineComponent* SplineComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Spline Settings")
+	bool bSnapToGround = true;
+
+	UFUNCTION(CallInEditor, Category = "Spline")
+	void SnapAllPointsToGround();
 };
